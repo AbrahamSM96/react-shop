0b80bdcc04cc247009cb9ad3312b1dd6f7b0a541
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-01-04 18:34:52 -0600

    add: add components, containers, pages and styles to all project with the assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
-export const Home = () => {
+import ProductList from "../containers/ProductList";
+import { Header } from "../components/Header";
+const Home = () => {
   return (
-    <div>
-      <h1>React Shop</h1>
-    </div>
+    <>
+      <Header />
+      <ProductList />
+    </>
   );
 };
+
+export default Home;
